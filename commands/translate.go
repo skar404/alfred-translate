b321a953e543271dc42035bfc5f6ce1685d17c85
Denis Malin <denis@malina.page>
2021-09-12 07:18:17 +0300

    done

--- commands/translate.go
+++ commands/translate.go
@@ -12,19 +12,25 @@ import (
 func Translate() {
 	wf := global.WF
 
+	if global.Flag.Value == "" {
+		wf.NewItem("Enter translate text ... ")
+		return
+	}
+
 	for lang := range getConfigLanguage() {
 		log.Printf("Translate %s", lang)
 		info := utils.GetLangInfo(lang)
 
 		text, err := yandex_translate.TranslateText(global.Flag.Value, lang)
 		if err != nil {
-			wf.NewItem(fmt.Sprintf("Error %s", err))
+			wf.NewItem(fmt.Sprintf("Error translate, err: %s", err)).
+				Subtitle(fmt.Sprintf("Full err: %s", err)).Valid(false)
 			return
 		}
 
 		wf.NewItem(text).Icon(&aw.Icon{
 			Value: fmt.Sprintf("./country-flags/img/%s.png", info.ImgCode),
 			Type:  aw.IconTypeImage,
-		})
+		}).Valid(true).Var("text", text)
 	}
 }
